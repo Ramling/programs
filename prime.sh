653e381d6bda72dfcd1c5c514dd56c14ed958721
root <root@localhost.localdomain>
2016-07-24 07:58:24 +0530

    Prime complete

--- prime.sh
+++ prime.sh
@@ -4,7 +4,20 @@ echo "Program for prime no"
 echo "Enter the no"
 read no
 
-x=2
+x=2 
+
+while [ `expr $x -lt $no ]
+do
+     if [ `expr $no % 2` -eq 0 ]
+     then
+        echo "Number is not prime "
+        exit
+     fi
+     x=`expr $x + 1`
+done
+
+echo "Number is prime"
+
 
 
 
